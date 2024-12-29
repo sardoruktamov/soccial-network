3c1652ef25b9cadf7d650287d6d433ea86bc4e06
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-12-29 20:41:56 +0500

    update ApplicationTests

--- CODEUZ/api.giybat.uz/src/test/java/api/giybat/uz/api/giybat/uz/ApplicationTests.java
+++ CODEUZ/api.giybat.uz/src/test/java/api/giybat/uz/api/giybat/uz/ApplicationTests.java
@@ -1,13 +1,20 @@
 package api.giybat.uz.api.giybat.uz;
 
+import api.giybat.uz.api.giybat.uz.service.SmsSendService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private SmsSendService smsSendService;
+
 	@Test
-	void contextLoads() {
+	void contextLoads(){
+		smsSendService.getToken();
 	}
 
 }
