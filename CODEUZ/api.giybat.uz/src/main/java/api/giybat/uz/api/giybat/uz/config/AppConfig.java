b4a4dfb0ce60f4dce2dea0c156dfb31a3a8750be
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-12-29 20:39:57 +0500

    update AppConfig

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/config/AppConfig.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/config/AppConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Locale;
 
@@ -17,6 +18,11 @@ public class AppConfig {
         messageSource.setDefaultEncoding("UTF-8");
         messageSource.setDefaultLocale(new Locale("UZ"));
         return messageSource;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
 }
