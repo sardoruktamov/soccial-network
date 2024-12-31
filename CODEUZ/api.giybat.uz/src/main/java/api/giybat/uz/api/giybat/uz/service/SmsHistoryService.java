11421aa14e54b9c840802e8473ad759d21818c28
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2024-12-31 12:03:12 +0500

    complated SMS verification

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/service/SmsHistoryService.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/service/SmsHistoryService.java
@@ -1,25 +1,30 @@
 package api.giybat.uz.api.giybat.uz.service;
 
 import api.giybat.uz.api.giybat.uz.entity.SmsHistoryEntity;
+import api.giybat.uz.api.giybat.uz.enums.AppLanguage;
 import api.giybat.uz.api.giybat.uz.enums.SmsType;
+import api.giybat.uz.api.giybat.uz.exps.AppBadException;
 import api.giybat.uz.api.giybat.uz.repository.SmsHistoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class SmsHistoryService {
 
     @Autowired
     private SmsHistoryRepository smsHistoryRepository;
-
+    @Autowired
+    private ResourceBundleService bundleService;
     public void created(String phone, String message, String code, SmsType smsType){
         SmsHistoryEntity entity = new SmsHistoryEntity();
         entity.setPhoneNumber(phone);
         entity.setMessage(message);
         entity.setCode(code);
         entity.setSmsType(smsType);
+        entity.setAttemptCount(0);
         entity.setCreatedDate(LocalDateTime.now());
         smsHistoryRepository.save(entity);
     }
@@ -28,4 +33,28 @@ public class SmsHistoryService {
         LocalDateTime now = LocalDateTime.now();
         return smsHistoryRepository.countByPhoneNumberAndCreatedDateBetween(phone,now.minusMinutes(1),now);
     }
+
+    public void check(String phoneNumber, String code, AppLanguage lang){
+        Optional<SmsHistoryEntity> optional = smsHistoryRepository.findTop1ByPhoneNumberOrderByCreatedDateDesc(phoneNumber);
+        if (optional.isEmpty()){
+            throw new AppBadException(bundleService.getMessage("verification.failed",lang));
+        }
+        // chacking code
+        SmsHistoryEntity entity = optional.get();
+
+        if (entity.getAttemptCount() >= 3){
+            throw new AppBadException(bundleService.getMessage("number.attempts.expired",lang));
+        }
+        if (!entity.getCode().equals(code)){
+            smsHistoryRepository.updateAttemptCount(entity.getId());
+            throw new AppBadException(bundleService.getMessage("verification.failed",lang));
+        }
+        //check time
+        LocalDateTime expDate = entity.getCreatedDate().plusMinutes(2);
+        if (LocalDateTime.now().isAfter(expDate)) {                               //agar hozirgi vaqt expDatedan keyin(katta yani o'tib ketgan) bo'lsa,
+            throw new AppBadException(bundleService.getMessage("time.sms.code.expired",lang));   // yani 2minut vaqt o'tib ketgan bo'lsa
+
+        }
+    }
+
 }
