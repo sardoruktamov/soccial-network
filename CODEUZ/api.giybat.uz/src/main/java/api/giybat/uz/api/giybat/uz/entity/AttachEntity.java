fcf929a3dc9a1982191e3486f286c13ffc1dfaf0
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-01-20 22:14:33 +0500

    add field AttachId

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/entity/AttachEntity.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/entity/AttachEntity.java
@@ -1,17 +1,20 @@
 package api.giybat.uz.api.giybat.uz.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "attach")
+@Getter
+@Setter
 public class AttachEntity {
 
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private String id;
 
     @Column(name = "path")
     private String path;
