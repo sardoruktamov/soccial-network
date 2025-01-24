1d6100c204aacbb0e71bf83073e7798fbcb133ad
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-01-24 21:58:06 +0500

    update AttachRepository

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/repository/AttachRepository.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/repository/AttachRepository.java
@@ -1,9 +1,17 @@
 package api.giybat.uz.api.giybat.uz.repository;
 
 import api.giybat.uz.api.giybat.uz.entity.AttachEntity;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AttachRepository extends CrudRepository<AttachEntity,String> {
+
+    @Transactional
+    @Modifying
+    @Query("update AttachEntity set visible = false where id = ?1")
+    void changeVisible(String id);
 }
