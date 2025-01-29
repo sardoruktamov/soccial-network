e84d43601e97e414613094b79c264f7659b7b9d4
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-01-29 21:49:12 +0500

    update for swagger

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/controller/PostController.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/controller/PostController.java
@@ -2,6 +2,7 @@ package api.giybat.uz.api.giybat.uz.controller;
 
 import api.giybat.uz.api.giybat.uz.dto.PostDTO;
 import api.giybat.uz.api.giybat.uz.util.SpringSecurityUtil;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/posts")
 @Slf4j
+@Tag(name = "PostController", description = "API list for working with Post")
 public class PostController {
 
     @PostMapping("/create")
