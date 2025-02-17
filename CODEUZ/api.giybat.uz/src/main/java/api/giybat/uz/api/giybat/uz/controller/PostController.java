df4f257547a7f0d14eefe857356b92f5ac12ad61
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-02-17 22:42:28 +0500

    update PostController

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/controller/PostController.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/controller/PostController.java
@@ -1,13 +1,21 @@
 package api.giybat.uz.api.giybat.uz.controller;
 
-import api.giybat.uz.api.giybat.uz.dto.PostDTO;
+import api.giybat.uz.api.giybat.uz.dto.post.PostCreateDTO;
+import api.giybat.uz.api.giybat.uz.dto.post.PostDTO;
+import api.giybat.uz.api.giybat.uz.dto.post.PostFilterDTO;
+import api.giybat.uz.api.giybat.uz.service.PostService;
 import api.giybat.uz.api.giybat.uz.util.SpringSecurityUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.websocket.server.PathParam;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -15,11 +23,45 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "PostController", description = "API list for working with Post")
 public class PostController {
 
-    @PostMapping("/create")
-    public String create(@RequestBody PostDTO postDTO){
-        log.info(SpringSecurityUtil.getCurrentProfile().getName() + " " +  SpringSecurityUtil.getCurrentProfile().getUsername());
-        System.out.println(SpringSecurityUtil.getCurrentProfile());
-        System.out.println(SpringSecurityUtil.getCurrentUserId());
-        return "ooooooooooookkkkkkk!!!!!!";
+    @Autowired
+    private PostService postService;
+
+    @PostMapping("")
+    @Operation(summary = "Create Post", description = "Api used for Post creation ")
+    public ResponseEntity<PostDTO> create(@Valid @RequestBody PostCreateDTO dto){
+        return ResponseEntity.ok(postService.create(dto));
+    }
+
+    @GetMapping("/profile")
+    @Operation(summary = "Get Post List", description = "Api used for Get All Profile Post List")
+    public ResponseEntity<List<PostDTO>> postListByProfile(){
+        return ResponseEntity.ok(postService.getProfilePostList());
+    }
+
+    @GetMapping("/public/{id}")
+    @Operation(summary = "Get Post by ID", description = "Api used for Get Post by id")
+    public ResponseEntity<PostDTO> postById(@PathVariable("id") String id){
+        return ResponseEntity.ok(postService.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update Post", description = "Api used for Post update")
+    public ResponseEntity<PostDTO> update(@PathVariable("id") String id,
+                                            @Valid @RequestBody PostCreateDTO dto){
+        return ResponseEntity.ok(postService.update(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete Post", description = "Api used for Post Delete")
+    public ResponseEntity<Boolean> delete(@PathVariable("id") String id){
+        return ResponseEntity.ok(postService.delete(id));
+    }
+
+    @PostMapping("/public/filter")
+    @Operation(summary = "Post public filter", description = "Api used for Post filter ")
+    public ResponseEntity<Page<PostDTO>> filter(@Valid @RequestBody PostFilterDTO dto,
+                                                @RequestParam(value = "page", defaultValue = "1") int page,
+                                                @RequestParam(value = "size", defaultValue = "10") int size){
+        return ResponseEntity.ok(postService.filter(dto, page-1, size));
     }
 }
