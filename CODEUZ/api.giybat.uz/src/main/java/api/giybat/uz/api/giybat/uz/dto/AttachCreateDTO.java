dbe6448108e6287176fbeec2576a33a7c64e55e5
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-02-17 22:43:22 +0500

    update AttachCreateDTO

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/dto/AttachCreateDTO.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/dto/AttachCreateDTO.java
@@ -1,6 +1,7 @@
 package api.giybat.uz.api.giybat.uz.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,6 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 public class AttachCreateDTO {
-    @NotNull(message = "ID required")
+    @NotBlank(message = "ID required")
     private String id;
 }
