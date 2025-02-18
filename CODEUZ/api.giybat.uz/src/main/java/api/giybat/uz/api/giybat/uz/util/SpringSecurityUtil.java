4b9743ab92985684c8aea3de77ae295ce37acd8a
Sardorbek <73293096+sardoruktamov@users.noreply.github.com>
2025-02-18 21:26:20 +0500

    update SpringSecurityUtil

--- CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/util/SpringSecurityUtil.java
+++ CODEUZ/api.giybat.uz/src/main/java/api/giybat/uz/api/giybat/uz/util/SpringSecurityUtil.java
@@ -2,6 +2,7 @@ package api.giybat.uz.api.giybat.uz.util;
 
 
 import api.giybat.uz.api.giybat.uz.config.CustomUserDetails;
+import api.giybat.uz.api.giybat.uz.enums.ProfileRole;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -17,5 +18,11 @@ public class SpringSecurityUtil {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
         return user.getId();
+    }
+
+    public static boolean hazRole(ProfileRole role) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream()
+                .anyMatch(pr -> pr.getAuthority().equals(role.name()));
     }
 }
